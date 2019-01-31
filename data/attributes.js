58e247f692447cab48fa5f5237db4278a5aa4b60
Richard Morrell Hoeata <riki@craftsmoothie.co.nz>
2019-02-01 11:41:19 +1300

    capitals

--- data/attributes.js
+++ data/attributes.js
@@ -1,6 +1,6 @@
 const attributes = {
   region: ['Europe', 'Asia', 'Africa', 'Oceania', 'Americas'],
-  elements: ['Wind', 'Water', 'Earth'],
+  element: ['Wind', 'Water', 'Earth'],
   habitat: ['Lake', 'Mountains', 'River', 'Forest', 'Cave']
 }
 
