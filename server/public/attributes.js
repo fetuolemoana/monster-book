6ac8242661d8e7b785229bce96df0f2b8cb3f8b4
Fetu-o-le-moana Teuila Tamapeau <fetu.tamapeau@gmail.com>
2019-01-31 18:25:57 +1300

    Add data with team

---
+++ server/public/attributes.js
@@ -0,0 +1,7 @@
+const attributes = {
+  region: ['Europe', 'Asia', 'Africa', 'Oceania', 'Americas'],
+  element: ['Wind', 'Water', 'Earth'],
+  habitat: ['Lake', 'Mountains', 'River', 'Forest', 'Cave']
+}
+
+export default attributes
---
+++ server/public/monsters.js
@@ -0,0 +1,49 @@
+const monsters = [
+  {
+    id: 1,
+    name: 'Nessie',
+    description: 'Long neck dinosaur',
+    image: 'nessie.jpg',
+    region: 'Europe',
+    element: 'Water',
+    habitat: 'Lake'
+  },
+  {
+    id: 2,
+    name: 'Yeti',
+    description: 'Furry giant',
+    image: 'yeti.jpg',
+    region: 'Asia',
+    element: 'Earth',
+    habitat: 'Mountain'
+  },
+  {
+    id: 3,
+    name: 'Tikoloshe',
+    description: 'Sprite',
+    image: 'tikoloshe.jpg',
+    region: 'Africa',
+    element: 'Water',
+    habitat: 'River'
+  },
+  {
+    id: 4,
+    name: 'Jersey devil',
+    description: 'a flying biped with hooves, but there are many variations. The common description is that of a kangaroo-like or wyvern-like creature with a goat- or horse-like head, leathery bat-like wings, horns, small arms with clawed hands, cloven hooves and a forked tail. It has been reported to move quickly and often is described as emitting a blood-curdling scream.',
+    image: 'jersey.jpg',
+    region: 'Americas',
+    element: 'Wind',
+    habitat: 'Forest'
+  },
+  {
+    id: 5,
+    name: "Fe'e",
+    description: 'The coolest octopus',
+    image: 'fee.jpg',
+    region: 'Oceania',
+    element: 'Water',
+    habitat: 'Cave'
+  }
+]
+
+export default monsters
