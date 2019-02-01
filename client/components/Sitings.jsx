a9c7f376efe4e35ee2994cd41dca972a1f97eb27
Richard Morrell Hoeata <riki@craftsmoothie.co.nz>
2019-02-01 15:51:09 +1300

    Added submit form to add siting

--- client/components/Sitings.jsx
+++ client/components/Sitings.jsx
@@ -1,23 +1,51 @@
 import React from 'react'
-
 import sitings from '../../data/sitings'
 
-const Sitings = (props) => {
-  const id = props.monsterId
-  const monsterSitings = sitings.filter(siting => {
-    return Number(id) === siting.monsterId
-  })
-  return (
-    <div>
-      <h1>Monster Sitings</h1>
+// const Sitings = (props) => {
 
-      <ul className="attr-ul">
-        {monsterSitings.map((siting, index) => {
-          return <li className="attr-li" key={index}>{siting.description}</li>
-        })}
-      </ul>
-    </div>
-  )
+class Sitings extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      newSiting: '',
+      allSitings: sitings
+    }
+    this.addSiting = this.addSiting.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  addSiting (evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
+
+  handleSubmit () {
+    this.state.allSitings.push(this.state.newSiting)
+  }
+
+  render () {
+    const id = this.props.monsterId
+    const monsterSitings = this.state.allSitings.filter(siting => {
+      return Number(id) === siting.monsterId
+    })
+    return (
+      <div>
+        <h1>Monster Sitings</h1>
+
+        <ul className="attr-ul">
+          {monsterSitings.map((siting, index) => {
+            return <li className="attr-li" key={index}>{siting.description}</li>
+          })}
+        </ul>
+
+        <form>
+          <input name='newSiting' type='text' value={this.state.newSiting} onChange={this.addSiting}/>
+          <button type='submit' onSubmit={this.handleSubmit}>Submit </button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Sitings
