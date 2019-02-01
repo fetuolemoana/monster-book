133d0509fc85680013c0b84c8900c88eb16d2078
nzbenji <benlittle53@gmail.com>
2019-02-01 14:13:46 +1300

    added monster images

--- client/components/Monster.jsx
+++ client/components/Monster.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import monsters from '../../data/monsters'
 
-const monsterImage = '/images/nessie.jpg'
-
 const Monster = (props) => {
   const {monster, id} = props.match.params
   const chosenMonster = monsters.find(monster => {
     return Number(id) === monster.id
   })
+  console.log(chosenMonster.image)
   return (
     <div>
-      <img src={monsterImage}/>
+      <img src={chosenMonster.image}/>
       <h1>{chosenMonster.name}</h1>
       <h3>{chosenMonster.region}</h3>
       <h3>{chosenMonster.element}</h3>
