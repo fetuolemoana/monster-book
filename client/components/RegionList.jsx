2d350ef3dc4f280e33f7325dc8016c3dc539abeb
Jessica Morgan <jess-morgan1@outlook.com>
2019-02-01 14:10:17 +1300

    Added css for attributes

--- client/components/RegionList.jsx
+++ client/components/RegionList.jsx
@@ -3,11 +3,14 @@ import attributes from '../../data/attributes'
 import {Link} from 'react-router-dom'
 
 const RegionList = () => {
+  const capStyle = {
+    textTransform: 'capitalize'
+  }
   return (
-    <div>
-      <h1>Regions</h1>
-      <ul>
-        {attributes.region.map(reg => { return <li key={reg}><Link to={`/attribute/region/${reg}`}>{reg}</Link></li> })}
+    <div className="padded-div" style={capStyle}>
+      <h1 className='heading'>Regions</h1>
+      <ul className="attr-ul">
+        {attributes.region.map(reg => { return <li className="attr-li" key={reg}><Link to={`/attribute/region/${reg}`}>{reg}</Link></li> })}
       </ul>
     </div>
   )
