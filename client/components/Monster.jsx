0cba716956dafda92d7071d2c9597a0f6656977b
nzbenji <benlittle53@gmail.com>
2019-02-01 13:45:29 +1300

    Connect chosenMonster

--- client/components/Monster.jsx
+++ client/components/Monster.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import monsters from '../../data/monsters'
 
-
-
 const Monster = (props) => {
-  const id = props.match.params.id
+  const {monster, id} = props.match.params
+  console.log(id)
+  const chosenMonster = monsters.find(monster => {
+    return Number(id) === monster.id
+  })
   return (
     <div>
       <h1>Hello</h1>
-      <h1>{name}</h1>
-      <h1>{description}</h1>git 
+      <h1>{chosenMonster.name}</h1>
     </div>
   )
 }
 
 export default Monster
-
 
 // id
 // name
